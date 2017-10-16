db8e7c2ff44a4de4e34bf03ac02b9c358f812ef8
brosashazel <hazel_brosas@dlsu.edu.ph>
2017-10-16 20:56:57 +0800

    PYTHOOOON <3 + FB LOGIN API

---
+++ YOUapp/YOUapp/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url, include # Add include to the imports here
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^', include('clientapp.urls')) # tell django to read urls.py in example app
+]
