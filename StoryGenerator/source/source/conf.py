44a7dc53f33cb342b087d3c62149437eb655a3c7
hazelMescolare <hazel.mescolare@gmail.com>
2018-08-05 19:12:25 +0800

    Technical Manual - StoryGen

--- StoryGenerator/source/source/conf.py
+++ StoryGenerator/source/source/conf.py
@@ -126,11 +126,11 @@ htmlhelp_basename = 'StoryGeneratordoc'
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
-    # 'papersize': 'letterpaper',
+    'papersize': 'a4paper',
 
     # The font size ('10pt', '11pt' or '12pt').
     #
-    # 'pointsize': '10pt',
+    'pointsize': '12pt',
 
     # Additional stuff for the LaTeX preamble.
     #
@@ -138,12 +138,13 @@ latex_elements = {
 
     # Latex figure (float) alignment
     #
-    # 'figure_align': 'htbp',
+    'figure_align': 'htbp',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
+
 latex_documents = [
     (master_doc, 'StoryGenerator.tex', 'Story Generator Documentation',
      'FB Persona', 'manual'),
@@ -155,7 +156,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'storygenerator', 'Story Generator Documentation',
+    (master_doc, 'storygen', 'Story Generator for YOU: A Persona-Based Life Story Generator Documentation',
      [author], 1)
 ]
 
@@ -166,7 +167,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'StoryGenerator', 'Story Generator Documentation',
+    (master_doc, 'StoryGenerator', 'Story Generator for YOU: A Persona-Based Life Story Generator Documentation',
      author, 'StoryGenerator', 'One line description of project.',
      'Miscellaneous'),
 ]
